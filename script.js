36824322aaad28ceab02cdf51e925073ef659575
pavlevuksanovic08 <pavlevuksanovic10@gmail.com>
2024-10-11 22:58:40 +0200

    Finishing first step, basic math functions

---
+++ script.js
@@ -0,0 +1,15 @@
+function add(first, second) {
+    return Number(first) + Number(second);
+}
+
+function subtract(first, second) {
+    return Number(first) - Number(second);
+}
+
+function multiply(first, second) {
+    return Number(first) * Number(second);
+}
+
+function add(first, second) {
+    return Number(first) / Number(second);
+}
